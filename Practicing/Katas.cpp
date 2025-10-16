4a576c67cd6b8b74bf07dcbd1b7326825d6a0b0a
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-10-16 23:48:19 +0300

    square_or_square_root_added

--- Practicing/Katas.cpp
+++ Practicing/Katas.cpp
@@ -1,5 +1,7 @@
 #include "Katas.h"
 #include <cmath>
+#include <vector>
+#include <iostream>
 
 int litres(double time) 
 {
@@ -17,4 +19,37 @@ double converter(int mpg)
 	double gallon = 4.54609188;
 	double mile = 1.609344;
 	return round(mpg / gallon * mile * 100.0) / 100.0;
+}
+
+std::vector<int> squareOrSquareRoot(const std::vector<int>& array)
+{
+	std::vector<int> res;
+	//for (size_t i = 0; i < array.size(); ++i)
+	//{
+	//	double temp = sqrt(array[i]);
+	//	if (std::floor(temp) == temp)
+	//		res.push_back(temp);
+	//	else
+	//		res.push_back(array[i] * array[i]);
+	//}
+
+	//for (auto it = array.begin(); it != array.end(); ++it)
+	//{
+	//	double temp = sqrt(*it);
+	//	if (std::floor(temp) == temp)
+	//		res.push_back(temp);
+	//	else
+	//		res.push_back(*it * *it);
+	//}
+
+	for (int x : array)
+	{
+		double temp = sqrt(x);
+		if (std::floor(temp) == temp)
+			res.push_back(temp);
+		else
+			res.push_back(x * x);
+	}
+
+	return res;
 }
