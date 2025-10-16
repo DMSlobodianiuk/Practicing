f51557a389a6494d9d5bacbdcd2c7a5e039dce87
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-10-16 22:18:18 +0300

    volume_of_cobuid_added

--- Practicing/Katas.h
+++ Practicing/Katas.h
@@ -1,3 +1,4 @@
 #pragma once
 
 int litres(double time);
+double getVolumeOfCuboid(double length, double width, double height);
--- Practicing/Main.cpp
+++ Practicing/Main.cpp
@@ -4,12 +4,6 @@ using namespace std;
 
 int main()
 {
-
-	cout << litres(1.1) << endl;
-	cout << litres(3) << endl;
-	cout << litres(6.7) << endl;
-	cout << litres(11.8) << endl;
-
 	system("pause");
 	return 0;
 }
