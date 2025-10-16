c8234c9d83517849b5669c47d2fa8e9814a039c0
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-10-16 21:33:10 +0300

    Add project files.

---
+++ Practicing/Main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int litres(double time);
+
+int main()
+{
+
+	cout << litres(1.1) << endl;
+
+	system("pause");
+	return 0;
+}
+
+int litres(double time) {
+
+	return 0;
+}
