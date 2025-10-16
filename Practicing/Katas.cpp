185ce75056dfcb3771ee971738b52b07b20b25cf
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-10-16 22:52:50 +0300

    miles_per_gallon_added

--- Practicing/Katas.cpp
+++ Practicing/Katas.cpp
@@ -1,4 +1,5 @@
 #include "Katas.h"
+#include <cmath>
 
 int litres(double time) 
 {
@@ -9,4 +10,11 @@ int litres(double time)
 double getVolumeOfCuboid(double length, double width, double height)
 {
 	return length*width*height;
+}
+
+double converter(int mpg)
+{
+	double gallon = 4.54609188;
+	double mile = 1.609344;
+	return round(mpg / gallon * mile * 100.0) / 100.0;
 }
