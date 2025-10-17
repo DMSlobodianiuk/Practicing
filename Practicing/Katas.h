581a84e6c40e63d24f273462da42011229b4a976
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-10-17 22:06:54 +0300

    count_positive_sum_negative_added

--- Practicing/Katas.h
+++ Practicing/Katas.h
@@ -5,3 +5,4 @@ int litres(double time);
 double getVolumeOfCuboid(double length, double width, double height);
 double converter(int mpg);
 std::vector<int> squareOrSquareRoot(const std::vector<int>& array);
+std::vector<int> countPositivesSumNegatives(std::vector<int> input);
