f4abcc2819771c068704c4594b993ae986f4838f
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-10-17 22:24:07 +0300

    string_to_number_added

--- Practicing/Katas.h
+++ Practicing/Katas.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <vector>
+#include <string>
 
 int litres(double time);
+
 double getVolumeOfCuboid(double length, double width, double height);
+
 double converter(int mpg);
+
 std::vector<int> squareOrSquareRoot(const std::vector<int>& array);
+
 std::vector<int> countPositivesSumNegatives(std::vector<int> input);
+
+int string_to_number(const std::string& s);
