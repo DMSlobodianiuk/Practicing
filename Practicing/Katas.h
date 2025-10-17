e76d97070a969cfdd0ebe93bf88cb21b025ae278
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-10-17 22:46:04 +0300

    divisible_by_added

--- Practicing/Katas.h
+++ Practicing/Katas.h
@@ -17,3 +17,5 @@ int string_to_number(const std::string& s);
 bool amIWilson(unsigned n);
 
 double twoDecimalPlaces(double n);
+
+std::vector<int> divisible_by(std::vector<int> numbers, int divisor);
