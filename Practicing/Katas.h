e506f2847c80143558947d29bb23589d708300a1
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-10-29 23:28:13 +0200

    stock_summary_added

--- Practicing/Katas.h
+++ Practicing/Katas.h
@@ -32,3 +32,4 @@ double mean(std::string town, const std::string &string);
 
 double variance(std::string town, const std::string& string);
 
+std::string stockSummary(std::vector<std::string>& lstOfArt, std::vector<std::string>& categories);
