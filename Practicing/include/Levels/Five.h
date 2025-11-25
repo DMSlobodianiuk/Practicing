04915871ac0d45fe4e68d910592625217b2de858
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-11-26 00:40:21 +0200

    updates_25_11_25

---
+++ Practicing/include/Levels/Five.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include <string>
+
+namespace five
+{
+
+	std::pair<long long, long long> gap(int g, long long m, long long n);
+
+	long zeros(long n);
+
+	unsigned long long perimeter(int n);
+
+	double solve(double m);
+
+	std::vector<long long> smallest(long long n);
+
+}
