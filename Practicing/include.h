c7e3a553701df955031cffe608b6df8f342728fe
DMSlobodinaiuk <bristlbeak205@gmail.com>
2025-12-01 17:25:57 +0200

    files checked

--- Practicing/include.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Menu.h"
-#include "Helper/HelperFunctions.h"
-#include "Levels/Five.h"
-#include "Levels/Six.h"
-#include "Levels/Seven.h"
-#include "Levels/Eight.h"
-#include "Kata/Kata.h"
