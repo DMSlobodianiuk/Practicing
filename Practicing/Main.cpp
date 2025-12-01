8449f80f0fb7a507be3cd4099dc50a9003a1a046
DMSlobodinaiuk <bristlbeak205@gmail.com>
2025-12-01 21:27:10 +0200

    little fix

--- Practicing/Main.cpp
+++ Practicing/Main.cpp
@@ -7,9 +7,9 @@
 
 int main()
 {	
-	TaskLitres task("Litres", "Calculate litres consumed over time at a rate of 0.5 litres per hour.");
+	//TaskLitres task("Litres", "Calculate litres consumed over time at a rate of 0.5 litres per hour.");
 
-	task.run();
+
 
 
 	return 0;
