e8c4ae004f392d24bc2bf09eddb690004a536243
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-12-02 13:00:53 +0200

    little clear

--- Practicing/Main.cpp
+++ Practicing/Main.cpp
@@ -2,13 +2,9 @@
 #include "include.h"
 #include "Menu.h"
 #include "Tasks/ITask.h"
-#include "Tasks/CodewarsTasks/SixLevel/TaskStockSummary.h"
 
 int main()
 {	
-	TaskStockSummary task;
-
-	task.run();
 
 	return 0;
 }
