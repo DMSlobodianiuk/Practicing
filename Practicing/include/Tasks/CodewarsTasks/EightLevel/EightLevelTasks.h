2c0024cddaafd22f7a6005abf5d19d2a1440dcf0
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-12-02 21:36:48 +0200

    taskslist_added

---
+++ Practicing/include/Tasks/CodewarsTasks/EightLevel/EightLevelTasks.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "TaskAmIWilson.h"
+#include "TaskConverter.h"
+#include "TaskCountPositivesSumNegatives.h"
+#include "TaskDivisibleBy.h"
+#include "TaskLitres.h"
+#include "TaskSquareOrSquareRoot.h"
+#include "TaskStringToNumber.h"
+#include "TaskTwoDecimalPlaces.h"
